00e222ff572ce37c095352e14d117b7f38c70901
Kevin Hines <kevhines@gmail.com>
2021-12-02 16:20:46 -0500

    displaying content via index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+// Import the React and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// create a react component
+const App = () => {
+    return <div>Hi There!</div>
+}
+
+// take component and show it on scren
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
